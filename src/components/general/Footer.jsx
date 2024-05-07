e9edd50fb2cba1fdb95d7e94a4ea89435156e2ff
irizow <irisrossell@gmail.com>
2024-05-07 19:51:48 +0800

    change fotoer image path

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -3,7 +3,7 @@ import './general.css'
 export default function Footer() {
     return (
         <div className="footer">
-            <p>Created by <a href="https://github.com/irizow">Irizow<img src="../public/github-mark-white.png" className="github-logo" /></a></p>
+            <p>Created by <a href="https://github.com/irizow">Irizow<img src="/public/github-mark-white.png" className="github-logo" /></a></p>
             
         </div>
     )
