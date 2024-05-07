cc354e830c64a823294b03618069899706927b24
irizow <irisrossell@gmail.com>
2024-05-07 18:18:29 +0800

    add export to PDF functionality

---
+++ src/utils/exportPDF.js
@@ -0,0 +1,34 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { domToPng } from "modern-screenshot";
+import DomToImage from "dom-to-image";
+
+
+
+export default function genPDF() {
+    let scale = 2;
+    let node = document.getElementById("preview-page");
+
+    const options = {
+    width: node.clientWidth * scale,
+    height: node.clientHeight * scale,
+    style: {
+    transform: 'scale('+scale+')',
+    transformOrigin: 'top left'
+    }
+    }
+    
+    DomToImage.toPng(node, options)
+    .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        var doc = new jsPDF();
+        doc.addImage(img.src, 'PNG', 0, 0, 210, 297)
+        doc.save();
+    })
+    .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+    });
+
+  
+  }
