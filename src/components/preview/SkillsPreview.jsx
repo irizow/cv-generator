2067fe94a715a442790e5d677e029cc7708ee92c
irizow <irisrossell@gmail.com>
2024-05-07 20:19:30 +0800

    import imgs

--- src/components/preview/SkillsPreview.jsx
+++ src/components/preview/SkillsPreview.jsx
@@ -1,8 +1,12 @@
 import './preview.css'
+import progress1 from '../../assets/1progress.png'
+import progress2 from '../../assets/2progress.png'
+import progress3 from '../../assets/3progress.png'
+import progress4 from '../../assets/4progress.png'
+import progress5 from '../../assets/5progress.png'
 
 
 export function SkillsPreview({skillsInfo, color}) {
-    const imgSrc = "../../src/assets/"
     let isBlack = (color==="rgb(0, 0, 0)" || color==="rgb(20, 7, 133)") ? true : false
     let invert = "invert(100%)"
     let styles = {
@@ -14,19 +18,19 @@ export function SkillsPreview({skillsInfo, color}) {
         let prof = info.proficiency
         console.log("proficiency" + prof)
         if(prof >= 0 && prof < 20) {
-            return "1progress.png"
+            return progress1
         }
         else if (prof >= 20 && prof < 40) {
-            return "2progress.png"
+            return progress2
         }
         else if (prof >= 40 && prof < 60) {
-            return "3progress.png"
+            return progress3
         }
         else if (prof>= 60 && prof < 85) {
-            return "4progress.png"
+            return progress4
         }
         else {
-            return "5progress.png"
+            return progress5
         }
     }
     return (
@@ -35,7 +39,7 @@ export function SkillsPreview({skillsInfo, color}) {
             {skillsInfo.map((info, index) =>
             <div key={index} className="skill-container">
             <p>{info.skill}</p>
-            <img src={imgSrc + renderCondition(info)} style ={isBlack? {filter: invert} : null}></img>
+            <img src={renderCondition(info)} style ={isBlack? {filter: invert} : null}></img>
             {console.log("img src + render " + imgSrc + renderCondition)}
             </div>)}
 
